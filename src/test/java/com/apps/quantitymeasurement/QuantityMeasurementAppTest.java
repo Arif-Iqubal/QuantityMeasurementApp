dc030cec9b2fcf48594d5e2230656c4301f22387
Arif-Iquba'l <arif04102001@gmail.com>
2026-02-23 23:04:25 +0530

    [Arif Iqubal] : Updated test cases to use unified Length class and added cross-unit equality and inequality validations

--- src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -3,117 +3,78 @@ package com.apps.quantitymeasurement;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.apps.quantitymeasurement.QuantityMeasurementApp.Feet;
-import com.apps.quantitymeasurement.QuantityMeasurementApp.Inches;
-
 /**
- * Unit tests for Feet and Inches equality
+ * Unit tests for Unified Length system (UC3)
  */
 public class QuantityMeasurementAppTest {
 
-    // ============================
-    // FEET TEST CASES (UC1)
-    // ============================
-
     @Test
-    public void testFeetEquality_SameValue() {
+    public void testFeetEquality() {
 
-        Feet feet1 = new Feet(1.0);
-        Feet feet2 = new Feet(1.0);
+        Length length1 = new Length(1.0, Length.LengthUnit.FEET);
+        Length length2 = new Length(1.0, Length.LengthUnit.FEET);
 
-        assertTrue(feet1.equals(feet2),
+        assertTrue(length1.equals(length2),
                 "Expected same feet values to be equal");
     }
 
     @Test
-    public void testFeetEquality_DifferentValue() {
+    public void testInchesEquality() {
 
-        Feet feet1 = new Feet(1.0);
-        Feet feet2 = new Feet(2.0);
+        Length length1 = new Length(12.0, Length.LengthUnit.INCHES);
+        Length length2 = new Length(12.0, Length.LengthUnit.INCHES);
 
-        assertFalse(feet1.equals(feet2),
-                "Expected different feet values to NOT be equal");
-    }
-
-    @Test
-    public void testFeetEquality_NullComparison() {
-
-        Feet feet = new Feet(1.0);
-
-        assertFalse(feet.equals(null),
-                "Expected feet compared with null to return false");
-    }
-
-    @Test
-    public void testFeetEquality_DifferentClass() {
-
-        Feet feet = new Feet(1.0);
-        Inches inches = new Inches(12.0);
-
-        assertFalse(feet.equals(inches),
-                "Expected feet and inches comparison to return false");
-    }
-
-    @Test
-    public void testFeetEquality_SameReference() {
-
-        Feet feet = new Feet(1.0);
-
-        assertTrue(feet.equals(feet),
-                "Expected same reference comparison to return true");
-    }
-
-
-    // ============================
-    // INCHES TEST CASES (UC2)
-    // ============================
-
-    @Test
-    public void testInchesEquality_SameValue() {
-
-        Inches inch1 = new Inches(12.0);
-        Inches inch2 = new Inches(12.0);
-
-        assertTrue(inch1.equals(inch2),
+        assertTrue(length1.equals(length2),
                 "Expected same inches values to be equal");
     }
 
     @Test
-    public void testInchesEquality_DifferentValue() {
+    public void testFeetInchesComparison() {
 
-        Inches inch1 = new Inches(12.0);
-        Inches inch2 = new Inches(24.0);
+        Length feet = new Length(1.0, Length.LengthUnit.FEET);
+        Length inches = new Length(12.0, Length.LengthUnit.INCHES);
+
+        assertTrue(feet.equals(inches),
+                "Expected 1 foot to equal 12 inches");
+    }
+
+    @Test
+    public void testFeetInequality() {
+
+        Length feet = new Length(2.0, Length.LengthUnit.FEET);
+        Length inches = new Length(12.0, Length.LengthUnit.INCHES);
+
+        assertFalse(feet.equals(inches),
+                "Expected 2 feet to NOT equal 12 inches");
+    }
+
+    @Test
+    public void testInchesInequality() {
+
+        Length inch1 = new Length(10.0, Length.LengthUnit.INCHES);
+        Length inch2 = new Length(12.0, Length.LengthUnit.INCHES);
 
         assertFalse(inch1.equals(inch2),
-                "Expected different inches values to NOT be equal");
+                "Expected 10 inches to NOT equal 12 inches");
     }
 
     @Test
-    public void testInchesEquality_NullComparison() {
+    public void testCrossUnitInequality() {
 
-        Inches inch = new Inches(12.0);
+        Length feet = new Length(1.0, Length.LengthUnit.FEET);
+        Length inches = new Length(10.0, Length.LengthUnit.INCHES);
 
-        assertFalse(inch.equals(null),
-                "Expected inches compared with null to return false");
+        assertFalse(feet.equals(inches),
+                "Expected 1 foot to NOT equal 10 inches");
     }
 
     @Test
-    public void testInchesEquality_DifferentClass() {
+    public void testMultipleFeetComparison() {
 
-        Inches inch = new Inches(12.0);
-        Feet feet = new Feet(1.0);
+        Length feet = new Length(2.0, Length.LengthUnit.FEET);
+        Length inches = new Length(24.0, Length.LengthUnit.INCHES);
 
-        assertFalse(inch.equals(feet),
-                "Expected inches and feet comparison to return false");
+        assertTrue(feet.equals(inches),
+                "Expected 2 feet to equal 24 inches");
     }
-
-    @Test
-    public void testInchesEquality_SameReference() {
-
-        Inches inch = new Inches(12.0);
-
-        assertTrue(inch.equals(inch),
-                "Expected same reference comparison to return true");
-    }
-
 }
