2240a03783ca1b1076f8525d910cd05816de5613
Arif-Iquba'l <arif04102001@gmail.com>
2026-02-25 00:01:48 +0530

    [Arif Iqubal] : added UC11: Volume Measurement Equality, Conversion, and Addition (Litre, Millilitre, Gallon)

--- src/main/java/com/apps/quantitymeasurement/InvalidUnitMeasurementException.java
+++ src/main/java/com/apps/quantitymeasurement/InvalidUnitMeasurementException.java
@@ -1,6 +1,12 @@
+
 package com.apps.quantitymeasurement;
 
 public class InvalidUnitMeasurementException extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public InvalidUnitMeasurementException(String msg) {
 		super(msg);
 	}
